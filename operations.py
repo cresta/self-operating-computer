7b24f070f80ab0f9b8dbd5aa84c9050431204731
Tianyang Hu <89424990+tyhu05@users.noreply.github.com>
2024-02-07 21:48:22 -0800

    Revert "adjust prompt and format the code"

--- operations.py
+++ operations.py
@@ -1,13 +1,9 @@
 from operate.main import main
 
-
 def take_notes_in_google_docs(notes: str):
-    prompt = f"Open google docs with a new doc. Input \"{
-        notes}\" Change title to \"Notes\". Close after saving it."
+    prompt = f"Open google docs with a new doc. Input \"{notes}\" Change title to \"Notes\". Close after saving it."
     main(model="gpt-4-with-ocr", terminal_prompt=prompt)
 
-
 def google_search(q: str):
-    prompt = f"Chrome app is already opened on the desktop, please create a new tab, and search {
-        q} in the search bar"
+    prompt = f"Open Chrome and search {q} in the search bar"
     main(model="gpt-4-with-ocr", terminal_prompt=prompt)
